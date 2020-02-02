35013a173bbf69603df7b9130bcb63e07067eb55
William Martin <35669235+CominAtYou@users.noreply.github.com>
2020-02-02 15:00:14 -0600

    Update index.js

--- index.js
+++ index.js
@@ -19,28 +19,35 @@ bot.on('message', function(message){
 
 // These lines (apart from the ones that have remember) have been commented since my bot is not using them. If you want to use them, remove the '//' from every line EXCEPT the ones that have "Remember the semicolon."
 
-// bot.on('message', function(message){
-//  if(message.content == 'daunte')
-//	{
-//		message.reply("Daunte? More like Cuntae")
-//	}
-// });
+bot.on('message', function(message){
+  if(message.content == 'daunte')
+	{
+		message.reply("Daunte? More like Cuntae")
+	}
+});
 // Remember the semicolon if you are adding more functions under this one; if thre are none under this one, delete it.
 
-// bot.on('message', function(message){
-//	if(message.content == 'Daunte')
-//	{
-//		message.reply("Daunte? More like Cuntae")
-//	}
-// })
+bot.on('message', function(message){
+  if(message.content == 'Daunte')
+	{
+		message.reply("Daunte? More like Cuntae")
+	}
+ });
 // Remember the semicolon if you are adding more functions under this one; if thre are none under this one, delete it.
 
-// bot.on('message', function(message){
-//    if(message.author.username == 'Lucas')
-//    {
-//        message.reply("Lucas? More like Lickass")
-//    }
-// });
+bot.on('message', function(message){
+    if(message.author.username == 'Lucas')
+    {
+        message.reply("Lucas? More like Lickass")
+    }
+});
+
+bot.on('message', function(message){
+    if(message.author.username == 'Ksap')
+    {
+        message.reply("FURRY")
+    }
+})
 // Remember the semicolon if you are adding more functions under this one; if thre are none under this one, delete it.
 
 //bot.on('message', function(message){
