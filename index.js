58daacc20ad4bb94627ec37037f91551e050db47
William M <35669235+CominAtYou@users.noreply.github.com>
2020-05-14 11:45:09 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,59 +1,43 @@
 const Discord = require('discord.js');
-const bot = new Discord.Client();
+const client = new Discord.Client();
 const TOKEN = 'token'
 
-bot.on('message', function(message){
+client.on('message', function(message){
     if(message.content == 'Lucas')
     {
-        message.reply("Lucas? More like Lickass")
+        message.channel.output("Lucas? More like Lickass")
     }
 });
 
-bot.on('message', function(message){
+client.on('message', function(message){
 	if(message.content == 'lucas')
 	{
-		message.reply("Lucas? More like Lickass")
+		message.channel.output("Lucas? More like Lickass")
 	}
 })
 // Remember the semicolon if you are adding more functions under this one; if thre are none under this one, delete it.
 
-bot.on('message', function(message){
+client.on('message', function(message){
   if(message.content == 'daunte')
 	{
-		message.reply("Daunte? More like Cuntae")
+		message.channel.output("Daunte? More like Cuntae")
 	}
 });
 // Remember the semicolon if you are adding more functions under this one; if thre are none under this one, delete it.
 
-bot.on('message', function(message){
+client.on('message', function(message){
   if(message.content == 'Daunte')
 	{
-		message.reply("Daunte? More like Cuntae")
+		message.channel.output("Daunte? More like Cuntae")
 	}
  });
 // Remember the semicolon if you are adding more functions under this one; if thre are none under this one, delete it.
 
-bot.on('message', function(message){
+client.on('message', function(message){
     if(message.author.username == 'Lucas')
     {
-        message.reply("Lucas? More like Lickass")
+        message.channel.output("Lucas? More like Lickass")
     }
 });
 
-bot.on('message', function(message){
-    if(message.author.username == 'Ksap')
-    {
-        message.reply("FURRY")
-    }
-})
-// Remember the semicolon if you are adding more functions under this one; if thre are none under this one, delete it.
-
-//bot.on('message', function(message){
-//    if(message.author.username == 'CominAtYou')
-//    {
-//        message.reply("I'm **cominatyou** with this message")
-//    }
-// })
-// Remember the semicolon if you are adding more functions under this one; if thre are none under this one, delete it.
-
-bot.login('token');
+client.login('token');
