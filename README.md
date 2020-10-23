60c888ee192b47e105388aee360f0cca792c0d8e
CominAtYou <35669235+cominatyou@users.noreply.github.com>
2020-10-23 16:10:39 -0500

    Changed everything to actually be useful

--- README.md
+++ README.md
@@ -1,67 +1,8 @@
-# LucasBot
-Some dumb bot I made because my friend wouldn't stop making puns out of my username
+# Whomst
 
-## Getting set up
-First, clone the repo:
-
-
-`$ git clone https://github.com/CominAtYou/LucasBot`
-
-
-Then, install [node.js](https://nodejs.org).
-
-
-Now, we need to tell Discord you're making a bot.
-
-
- - Go to the [Discord Developer Portal](https://discordapp.com/developers) and click 'New Application', and name it.
-
-
- - Click "Bot" in the side menu, and then click 'Add Bot', and confirm.
-
-
- - Change your bot's name and picture (if you want), and copy its token.
-
-
-After that, navigate to the repo you cloned earlier:
-
-
-`$ cd <PATH/TO/REPO>`
-
-
-Then, remove the unneeded package.json since we will be making this later:
-
-
-`$ rm package.json`
-
-
-Then, initalize node:
-
-
-`$ npm init`
-
-
-Fill out the fields that are presented to you, or press enter for each one to skip this step.
-
-
-Import the discord.js library to the project:
-
-
-`$ npm install discord.js --save`
-
-
-Then, open up index.js and fill in your bot token in the nessecary fields, and if you want, make changes to what the bot says and does. A helpful resource for things the bot can do is the [discord.js documentation](https://discord.js.org/#/docs/main/stable/general/welcome).
-
-
-To add the bot to your server, get your Client ID from the 'Home' tab in the side menu of the Discord Developer Portal, and paste this URL into your web browser `https://discordapp.com/oauth2/authorize?client_id=YOUR_CLIENT_ID_HERE&scope=bot&permissions=67496976`, replacing `YOUR_CLIENT_ID_HERE` with the Client ID you previously obtained.
-
-
-Finally, to run the bot:
-
-
-`$ node .`
-
-
-## Licensing and other mumbo jumbo
-
-You can use this repository however you want as long as it is in line with the [license](https://github.com/CominAtYou/LucasBot/blob/master/LICENSE). 
+If you're reading this:
+ - How
+ - Why
+ - Stop
+ 
+ Thanks
--- index.js
+++ index.js
@@ -1,43 +1,46 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const TOKEN = 'token'
+const serverIcon = 'https://cdn.discordapp.com/icons/685236709277040802/771cef9de1fb7c82d7ded91b06383eb1.webp?size=128'
+const config = require('./config.json');
 
 client.on('message', function(message){
-    if(message.content == 'Lucas')
-    {
-        message.channel.output("Lucas? More like Lickass")
+    if(message.content == '!rule respect' && message.member.roles.find(r => r.id === '766858374377504818') || message.member.roles.find(r => r.id === '769299680357122088')) {
+        message.delete();
+        const reqEmbed = {
+            title: "BE RESPECTFUL",
+            description: "Always be respectful to Moderator and our server members. Avoid starting unnessecary drama and toxicity.",
+            // timestamp = new Date(),
+            footer: {
+                text: "Read #rules-and-info for more info.",
+                icon_url: serverIcon
+            }
+        }
+        message.channel.send({ embed: reqEmbed });
+    } 
+    else if (message.content == '!rule spoilers' && message.member.roles.find(r => r.id == '766858374377504818') || message.member.roles.find(r => r.id === '769299680357122088')) {
+        message.delete();
+        const reqEmbed = {
+            title: "USE SPOILERS RESPECTIVELY",
+            description: "Use spoilers for new content posted to ensure that you do not spoil things for others. In addition, do not use spoilers to imply innapropriate content.",
+            footer: {
+                text: "Read #rules-and-info for more info.",
+                icon_url: serverIcon
+            }
+        }
+        message.channel.send({ embed: reqEmbed });
     }
 });
 
-client.on('message', function(message){
-	if(message.content == 'lucas')
-	{
-		message.channel.output("Lucas? More like Lickass")
-	}
-})
-// Remember the semicolon if you are adding more functions under this one; if thre are none under this one, delete it.
 
-client.on('message', function(message){
-  if(message.content == 'daunte')
-	{
-		message.channel.output("Daunte? More like Cuntae")
-	}
-});
-// Remember the semicolon if you are adding more functions under this one; if thre are none under this one, delete it.
-
-client.on('message', function(message){
-  if(message.content == 'Daunte')
-	{
-		message.channel.output("Daunte? More like Cuntae")
-	}
+client.on("ready", () =>{
+    console.log(`Logged in as ${client.user.tag}`);
+    client.user.setPresence({
+        status: "online",  // You can show online, idle, dnd
+        game: {
+            name: "the campfire",  // The message shown
+            type: "Listening" // PLAYING, WATCHING, LISTENING, STREAMING
+        }
+    });
  });
-// Remember the semicolon if you are adding more functions under this one; if thre are none under this one, delete it.
 
-client.on('message', function(message){
-    if(message.author.username == 'Lucas')
-    {
-        message.channel.output("Lucas? More like Lickass")
-    }
-});
-
-client.login('token');
+ client.login(config.token);
