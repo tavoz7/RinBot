8e8aedc830489480687893c7c090eb0855445d0e
CominAtYou <35669235+cominatyou@users.noreply.github.com>
2020-11-03 12:46:12 -0600

    bump version number

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 
 const { prefix, token, lastChannelID } = require('./config.json');
 var { updateInProgress, lastClientMessageID } = require('./config.json');
-var version = "0.6.2 - Pre-Release";
+var version = "0.6.3 - Pre-Release";
 var versionDate = "2 November 2020";
 const configFile = './config.json';
 const file = require(configFile);
