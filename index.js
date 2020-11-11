9a68189594a7415f795fd148c4bd04a644ff9bcd
William M <35669235+CominAtYou@users.noreply.github.com>
2020-11-11 08:43:09 -0600

    bump version number

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ client.commands = new Discord.Collection();
 const fs = require('fs');
 
 const { prefix, token, lastChannelID, updateInProgress, lastClientMessageID } = require('./config.json');
-var version = "0.7.1.2 - Pre-Release";
-var versionDate = "9 November 2020";
+var version = "0.7.1.3 - Pre-Release";
+var versionDate = "11 November 2020";
 const configFile = './config.json';
 const file = require(configFile);
 
