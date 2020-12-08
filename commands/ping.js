658343d8738c7a1dec347453592d1914faf045ee
CominAtYou <35669235+cominatyou@users.noreply.github.com>
2020-12-08 12:18:59 -0600

    color

--- commands/ping.js
+++ commands/ping.js
@@ -3,8 +3,10 @@ module.exports = {
     description: "",
     execute(message, client) {
         var reqEmbed = {
-            title: "🏓 Pong!",
-            description: `Bot latency is ${parseInt(message.createdTimestamp) - new Date().getTime()} ms\nDiscord API latency is ${Math.round(client.ws.ping)} ms`
+            title: ":ping_pong: Pong!",
+            description: `Bot latency is ${parseInt(message.createdTimestamp) - new Date().getTime()} ms\nDiscord API latency is ${Math.round(client.ws.ping)} ms`,
+            color: 0x24ACF2,
+            timestamp: new Date()
         }
         message.channel.send({embed: reqEmbed});
     }
