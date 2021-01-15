fc041647c6e55c948956a0ee06e1624d3201901b
CominAtYou <35669235+cominatyou@users.noreply.github.com>
2021-01-14 23:40:21 -0600

    Updated uuid

---
+++ commands/rps.js
@@ -0,0 +1,8 @@
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.execute = exports.description = exports.name = void 0;
+exports.name = "rps";
+exports.description = "Play rock paper scissors."; // F for senko.
+function execute(message) {
+    console.log("lmao");
+}
+exports.execute = execute;
---
+++ commands/rps.ts
@@ -0,0 +1,6 @@
+import Discord = require('discord.js');
+export const name = "rps"
+export const description = "Play rock paper scissors." // F for senko.
+export function execute(message: Discord.Message) {
+    console.log("lmao");
+}
