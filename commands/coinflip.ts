e9b2e8e9f48b0ad95b4b66a20bea0593758a0d21
CominAtYou <35669235+cominatyou@users.noreply.github.com>
2021-01-18 17:55:10 -0600

    added coinflip

--- commands/coinflip.ts
+++ commands/coinflip.ts
@@ -2,7 +2,7 @@ import Discord = require('discord.js');
 export const name = "coinflip";
 export const description = "Flip a coin";
 export function execute(message: Discord.Message) {
-    let choice = Math.floor(Math.random() * 6000 - 1 + 1);
+    let choice = Math.floor(Math.random() * (6000 - 1 + 1) + 1);
     if (choice === 420) {
         var sideEmbed = {
             color: 0x24ACF2,
