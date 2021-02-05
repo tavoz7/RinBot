4eb88bbfed539159446b66f06bbad918bc4ea73c
CominAtYou <35669235+cominatyou@users.noreply.github.com>
2021-02-05 12:00:47 -0600

    added say from accross channels

--- commands/say.ts
+++ commands/say.ts
@@ -2,9 +2,24 @@ import Discord = require('discord.js');
 
 export const name = "say";
 export const description = "Says something via the bot";
-export function execute(message: Discord.Message, args: string[]) {
+export async function execute(message: Discord.Message, client: Discord.Client, args: string[]) {
     if (message.author.id !== "245047280908894209") return;
-    message.delete();
-    message.channel.send(args.join(" "));
-    //h
+    if (args.includes('--channel')) {
+        if (/<#[0-9]{18}>/i.test(args[args.indexOf('--channel') + 1])) {
+            let channel = args.pop().replace("<", "").replace("#", "").replace(">", "");
+            args.pop();
+            try {
+                var targetChannel = await client.channels.fetch(channel);
+            } catch {
+                message.channel.send(":x: I don't seem to have access to that channel!");
+            }
+            (targetChannel as Discord.TextChannel).send(args.join(" "));
+        } else {
+            message.channel.send(":x: Invalid channel specified!");
+        }
+        message.react("👍");
+    } else {
+        message.delete();
+        message.channel.send(args.join(" "));
+    }
 }
--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const client = new Discord.Client();
 const commands = new Discord.Collection();
 const fs = require('fs');
 const { prefix, token, lastChannelID, updateInProgress, lastClientMessageID } = require('./config.json');
-var version = "0.17.1 - Pre-Release";
-var versionDate = "26 January 2020";
+var version = "0.17.2 - Pre-Release";
+var versionDate = "5 February 2021";
 const configFile = './config.json'
 const file = require('./config.json');
 const codeBlue = 0x24ACF2;
@@ -255,7 +255,7 @@ client.on('message', (message) => { // fires whenever a message is sent
             commands.get('quote').execute(message, args, client, db, prefix);
             break;
         case 'say':
-            commands.get('say').execute(message, args);
+            commands.get('say').execute(message, client, args);
     }
 });
 
